1e447de2302ae3302058251e127719e8d617138d
Benoit Lemaire <blemaire@walmartlabs.com>
2018-04-27 12:16:03 -0700

    Rename all source file extensions to .ts

--- ern-local-cli/src/index.dev.js
+++ ern-local-cli/src/index.dev.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
-if (process.env['BABEL_ENV'] !== 'coverage') {
+/*if (process.env['BABEL_ENV'] !== 'coverage') {
   // babelhook is already required through nyc command
   require('ern-util-dev/babelhook')
   require('./index.js').default()
 } else {
   require('./index.prod')
-}
+}*/
+require('tsconfig-paths/register')
+require('ts-node').register({})
+require('./index.ts').default()
